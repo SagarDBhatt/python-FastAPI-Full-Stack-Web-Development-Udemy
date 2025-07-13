757963cc0ebbc2c2fe40a4daca25c9b9d6e812fe
Sagar Bhatt <SagarBhatt@geico.com>
2025-07-13 15:58:48 -0600

    Enumerator in class

--- utilities/pydanticHelper.py
+++ utilities/pydanticHelper.py
@@ -1,19 +1,28 @@
+from collections import Counter
+
 from pydantic import BaseModel
+from enum import Enum
 
 
 class Engine(BaseModel):
     stroke: int
     age: int
 
+class Colour(Enum):
+    RED = "red"
+    YELLOW = "yellow"
+    GREEN =  "green"
 
 class Car(BaseModel):
     model: str
     make: int
-    color: str
+    color: Colour
     isSedan: bool = None
     engine: Engine
 
 
 engine_bmw = Engine(stroke=4, age=10)
-BMW = Car(model="x3", make=2007, color="red", isSedan=False, engine=engine_bmw)
+BMW = Car(model="x3", make=2007, color=Colour.RED, isSedan=False, engine=engine_bmw)
 print(BMW)
+
+print(f"Color of the BMW is {BMW.color.value}")
